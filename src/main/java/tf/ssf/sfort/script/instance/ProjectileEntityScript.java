8155485ffdc123f6c35169a82d1d03fcb2df1447
SFort <github@ssf.tf>
2023-04-30 20:20:54 +0200

    Add ProjectileEntityScript, ThrownEntityScript, ThrownItemEntityScript

---
+++ src/main/java/tf/ssf/sfort/script/instance/ProjectileEntityScript.java
@@ -0,0 +1,35 @@
+package tf.ssf.sfort.script.instance;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
+import tf.ssf.sfort.script.util.AbstractExtendablePredicateProvider;
+import tf.ssf.sfort.script.util.DefaultParsers;
+
+import java.util.function.Predicate;
+
+public class ProjectileEntityScript<T extends ProjectileEntity> extends AbstractExtendablePredicateProvider<T> {
+
+	public ProjectileEntityScript() {
+		help.put("~owner projectile_owner:ENTITY", "Require owner of projectile to match");
+	}
+
+	@Override
+	public Predicate<T> getLocalEmbed(String in, String script){
+		switch (in) {
+			case "owner" : case "projectile_owner" : {
+				final Predicate<Entity> predicate = DefaultParsers.ENTITY_PARSER.parse(script);
+				if (predicate == null) return null;
+				return entity -> predicate.test(entity.getOwner());
+			}
+			default : return null;
+		}
+	}
+}
